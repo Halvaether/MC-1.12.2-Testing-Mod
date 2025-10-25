fd77cc20673dab46fbdcdca4554d0147e7d34e98
Halvaether <halvaether@gmail.com>
2025-10-24 20:40:39 -0500

    First test. I have added an item to the game with this mod.
    I have practiced registering items, creating custom item classes, and
    implementing assets such as textures and language files into the game.

---
+++ src/main/java/objects/items/ItemBase.java
@@ -0,0 +1,32 @@
+package objects.items;
+
+import init.ItemInit;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import testmod.fmod.ExampleMod;
+import util.IHasModel;
+
+public class ItemBase extends Item implements IHasModel {
+
+	public ItemBase(String name) {
+		
+		setUnlocalizedName(name);
+		setRegistryName(name);
+		setCreativeTab(CreativeTabs.MISC);
+		
+		ItemInit.ITEMS.add(this);
+		
+	}
+	
+	
+	
+	@Override
+	public void registerModels() {
+		ExampleMod.proxy.registerItemRenderer(this, 0, "inventory");
+ 
+		
+	}
+	
+	
+	
+}
---
+++ src/main/java/testmod/fmod/ExampleMod.java
@@ -0,0 +1,50 @@
+package testmod.fmod;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import proxy.CommonProxy;
+
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
+public class ExampleMod
+{
+    public static final String MODID = "exmod";
+    public static final String NAME = "Example Mod";
+    public static final String VERSION = "1.0";
+    public static final String CLIENT = "proxy.ClientProxy";
+    public static final String COMMON = "proxy.CommonProxy";
+
+    private static Logger logger;
+
+    @SidedProxy(clientSide = CLIENT, serverSide = COMMON)
+    public static CommonProxy proxy;
+    
+    
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+    
+    @EventHandler
+    public void postInit(FMLInitializationEvent event)
+    {
+
+    }
+    
+
+    
+    
+}
